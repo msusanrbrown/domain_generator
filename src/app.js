341864e10a89364720df3da1c6a0d38ab2abfdef
msusanrbrown <sbrow120@fau.edu>
2021-07-05 20:14:38 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "your", "his"];
+  let adj = ["great", "big", "stylish", "silly"];
+  let noun = ["jogger", "cat", "car", "city"];
+  let ext = [".com", ".net", ".us", ".io"];
+  let finalName = "";
+  let ul = document.getElementById("list");
+  for (let i = 0; i < pronoun.length; i++) {
+    let str1 = pronoun[i];
+    for (let x = 0; x < adj.length; x++) {
+      let str2 = adj[x];
+      for (let y = 0; y < noun.length; y++) {
+        let str3 = noun[y];
+        for (let z = 0; z < ext.length; z++) {
+          let str4 = ext[z];
+
+          finalName = str1 + str2 + str3 + str4;
+          let li = document.createElement("li");
+          li.innerHTML = finalName;
+          ul.appendChild(li);
+          console.log(finalName);
+        }
+      }
+    }
+  }
 };
